667fe2fb6b66c10c80ecc0c72ba7ff66f6b0ddcd
Alejandro Zapata <keybladeschosen1@hotmail.com>
2018-05-28 19:01:34 -0700

    Adjacency node creation is underway, need to give them correct parents

--- CBN_model.py
+++ CBN_model.py
@@ -1,22 +1,47 @@
 # Dungeon causal Bayesian network model. Procedurally creates DAG based on given dungeon rooms and their intended behaviorself.
 
-from CBN_node_classes import Size, Entrance, Boss, Exit, Empty, Treasure, Trap
+from CBN_node_classes import Size, Entrance, Boss, Exit, Empty, Treasure, Trap, Adjacency
+import itertools
 
 class DAG:
     def __init__(self, nodes):
-        self.nodes = [Size(), Entrance(), Boss(), Exit()]
-        self.nodes.extend(nodes)
+        self.nodes = {'size': Size(), 'rooms': [Entrance(), Boss(), Exit()]}
+        self.nodes['rooms'].extend(nodes)
+        self.node_count = 0
+        for k, node in self.nodes.items():
+            if type(node) is list:
+                for n in node:
+                    self.node_count+=1
+            else:
+                self.node_count+=1
 
     def populate_and_orient(self):
-        return None
-        # not implemented :)
+        nodes = self.node_count - 2 # the size node does not count toward this total and the sequence is off by 1 (one).
+        adjacency_nodes = []
+
+        for node in self.nodes['rooms']:
+            # make size the parent to every room that isnt a default room
+            if node.type != "Entrance" and node.type != "Boss" and node.type != "Exit":
+                node.parents.append(self.nodes['size'])
+                self.nodes['size'].children.append(node)
+
+        pairs = list(itertools.combinations(self.nodes['rooms'], 2))
+        for pair in pairs:
+            adjacency_nodes.append(Adjacency(pair[0], pair[1]))
+
+        for i in adjacency_nodes:
+            i.write()
 
     def write(self):
-        for node in self.nodes:
-            node.write()
+        for k, node in self.nodes.items():
+            if type(node) is list:
+                for n in node:
+                    n.write()
+            else:
+                node.write()
 
 # Only instantiate non-default rooms (Treasure, Trap, Empty). Minimum requirement rooms are instantiated by default.
 
-model = DAG([Treasure(1), Trap(1), Empty(1)])
+model = DAG([Treasure(1), Treasure(2), Trap(1), Trap(2), Empty(1), Empty(2)])
 model.populate_and_orient()
 model.write()
--- CBN_node_classes.py
+++ CBN_node_classes.py
@@ -1,67 +1,74 @@
 # Class definitions for every node type in the CBN
 
-class Size:
+class Node:
+    def __init__(self):
+        self.type = "Generic"
+        self.children = []
+        self.parents = []
+
+    def write(self):
+        print('..........')
+        try:
+            print(self.type, self.id)
+        except AttributeError:
+            print(self.type)
+
+        parents = ''
+        for p in self.parents:
+            parents += f'{p.type} '
+        print("parents:", parents)
+        children = ''
+        for c in self.children:
+            children += f'{c.type} '
+        print("children:", children)
+
+class Size(Node):
     def __init__(self):
         self.type = "Size"
-        self.children = None
-        self.parents = None
+        self.children = []
+        self.parents = []
 
-    def write(self):
-        print(self.type)
-
-class Entrance:
+class Entrance(Node):
     def __init__(self):
         self.type = "Entrance"
-        self.children = None
-        self.parents = None
+        self.children = []
+        self.parents = []
 
-    def write(self):
-        print(self.type)
-
-class Boss:
+class Boss(Node):
     def __init__(self):
         self.type = "Boss"
-        self.children = None
-        self.parents = None
+        self.children = []
+        self.parents = []
 
-    def write(self):
-        print(self.type)
-
-class Exit:
+class Exit(Node):
     def __init__(self):
         self.type = "Exit"
-        self.children = None
-        self.parents = None
+        self.children = []
+        self.parents = []
 
-    def write(self):
-        print(self.type)
-
-class Empty:
+class Empty(Node):
     def __init__(self, id):
         self.type = "Empty"
         self.id = id
-        self.children = None
-        self.parents = None
+        self.children = []
+        self.parents = []
 
-    def write(self):
-        print(self.type, self.id)
-
-class Treasure:
+class Treasure(Node):
     def __init__(self, id):
         self.type = "Treasure"
         self.id = id
-        self.children = None
-        self.parents = None
+        self.children = []
+        self.parents = []
 
-    def write(self):
-        print(self.type, self.id)
-
-class Trap:
+class Trap(Node):
     def __init__(self, id):
         self.type = "Trap"
         self.id = id
-        self.children = None
-        self.parents = None
+        self.children = []
+        self.parents = []
 
-    def write(self):
-        print(self.type, self.id)
+class Adjacency(Node):
+    def __init__(self, room1, room2):
+        self.type = f'{room1.type} + {room2.type}'
+        self.children = []
+        self.parents = []
